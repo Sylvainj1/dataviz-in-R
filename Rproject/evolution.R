c0557fe36deb85261c8144b1091b561ebba97f2a
jeremys <jeremysurget77@gmail.com>
2019-10-16 11:31:06 +0200

    gathering data to display evolution

--- Rproject/evolution.R
+++ Rproject/evolution.R
@@ -10,23 +10,23 @@ data_evolution_station_long <- data_evolution_station %>% pivot_longer(c(Accessi
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    # title
-    titlePanel("test"),
-    # Main panel
-    mainPanel(plotOutput(outputId = "evolution_plot"))
+  # title
+  titlePanel("Evolution du nombre de borne de recharge en France par trimestre"),
+  # Main panel
+  mainPanel(plotOutput(outputId = "evolution_plot"))
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  output$evolution_plot <- renderPlot({
     
-    output$evolution_plot <- renderPlot({
-        
-        data_evolution_station_long %>%
-            ggplot(aes(x=Trimestre, y = Nombre , fill= Type)) +
-            geom_bar(stat= "identity")
-        
-    })
+    data_evolution_station_long %>%
+      ggplot(aes(x=Trimestre, y = Nombre , fill= Type)) +
+      geom_bar(stat= "identity")
     
+  })
+  
 }
 
 # Run the application 
