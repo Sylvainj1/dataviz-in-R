8072ea3f6224a59a9959fe729c2e049a799d5816
jeremys <jeremysurget77@gmail.com>
2019-10-16 14:35:17 +0200

    change to plotly to hover effect on geom_bar

--- Rproject/evolution.R
+++ Rproject/evolution.R
@@ -2,12 +2,11 @@ library(shiny)
 library(dplyr)
 library(tidyverse)
 library(tidyr)
-library(gganimate)
-library(gifski)
+library(plotly)
 
 
-data_evolution_station <- read.csv("nombre-total-de-points-de-charge.csv", sep=";",header = TRUE)
-data_evolution_station_long <- data_evolution_station %>% pivot_longer(c(Accessible.au.public,Particulier,Société),names_to = "Type", values_to = "Nombre")
+data_evolution_station <- read.csv("nombre-total-de-points-de-charge.csv", sep=";",header = TRUE) %>% rename(Public = Accessible.au.public)
+data_evolution_station_long <- data_evolution_station %>% pivot_longer(c(Public,Particulier,Société),names_to = "Type", values_to = "Nombre")
 
 
 
@@ -16,25 +15,20 @@ ui <- fluidPage(
   # title
   titlePanel("Evolution du nombre de borne de recharge en France par trimestre"),
   # Main panel
-  mainPanel(plotOutput(outputId = "evolution_plot"))
+  mainPanel(plotlyOutput(outputId = "evolution_plot"))
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-  output$evolution_plot <- renderPlot({
-    
+
+  output$evolution_plot <- renderPlotly({
+
     data_evolution_station_long %>%
       ggplot(aes(x=Trimestre, y = Nombre , fill= Type)) +
-      geom_bar(stat= "identity") +
-      transition_states(Trimestre, wrap = FALSE) +
-      shadow_mark() +
-      enter_grow() +
-      enter_fade()
-    
-    
+      geom_bar(stat= "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
+
   })
-  
+
 }
 
 # Run the application 
