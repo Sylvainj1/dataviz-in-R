298bc3c574b30d514066cb4d342880dc36e5d961
jeremys <jeremysurget77@gmail.com>
2019-10-16 11:54:42 +0200

    add animation (doesnt work on browser)

--- Rproject/evolution.R
+++ Rproject/evolution.R
@@ -2,6 +2,9 @@ library(shiny)
 library(dplyr)
 library(tidyverse)
 library(tidyr)
+library(gganimate)
+library(gifski)
+
 
 data_evolution_station <- read.csv("nombre-total-de-points-de-charge.csv", sep=";",header = TRUE)
 data_evolution_station_long <- data_evolution_station %>% pivot_longer(c(Accessible.au.public,Particulier,Société),names_to = "Type", values_to = "Nombre")
@@ -23,7 +26,12 @@ server <- function(input, output) {
     
     data_evolution_station_long %>%
       ggplot(aes(x=Trimestre, y = Nombre , fill= Type)) +
-      geom_bar(stat= "identity")
+      geom_bar(stat= "identity") +
+      transition_states(Trimestre, wrap = FALSE) +
+      shadow_mark() +
+      enter_grow() +
+      enter_fade()
+    
     
   })
   
