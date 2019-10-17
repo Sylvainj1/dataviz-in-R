184fc18a048190382e27a7723ad2dd3f2068e02d
sylvainjiang <sylvainjiang1@gmail.com>
2019-10-17 20:00:03 +0200

    barplot upgrade

--- Rproject/test.R
+++ Rproject/test.R
@@ -27,15 +27,26 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-
+  
   graph<-reactive({
-    ggplot(data = filter(data,Nom.de.la.région==input$region),
-           aes(x=Nom.du.département,
-               y = Puissance.délivrée,
-               fill=Condition.d.accès)) +
-      geom_bar(stat="identity", position=position_dodge() )+
-      labs(fill="Conditions d'accès",y="Puissance délivrée",x="Nombre de Points de charge")+
-      theme(axis.text.x = element_text(angle = 45,hjust = 1)) 
+    
+      filter(data,Nom.de.la.région==input$region) %>% 
+      group_by(Nom.de.la.région=input$region,Nom.du.département,Condition.d.accès) %>% 
+      summarize(Number=length(Nom.du.département)) %>% 
+      ggplot(aes(x=Nom.du.département,y=Number, fill = Condition.d.accès)) + 
+      geom_bar(stat = "identity")+
+      geom_text(aes(label=Number),color="black",position=position_stack(0.5))+
+      labs(fill="Conditions d'accès",y="Nombre de borne",x="Nom du département")+
+      theme(axis.text.x = element_text(angle = 45,hjust = 1))
+
+    
+    # ggplot(data = filter(data,Nom.de.la.région==input$region),
+    #        aes(x=Nom.du.département,
+    #            y = Puissance.délivrée,
+    #            fill=Condition.d.accès)) +
+    #   geom_bar(stat="identity", position=position_dodge() )+
+    #   labs(fill="Conditions d'accès",y="Puissance délivrée",x="Nombre de Points de charge")+
+    #   theme(axis.text.x = element_text(angle = 45,hjust = 1)) 
   })
   
   output$borneplot <- renderPlot({
