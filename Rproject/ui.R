08e7bebaf063b52e5db3437eb075794eb37b918e
jeremys <jeremysurget77@gmail.com>
2019-10-18 15:55:41 +0200

    add image + text + title

--- Rproject/ui.R
+++ Rproject/ui.R
@@ -1,5 +1,24 @@
 ui <- fluidPage(
   
+  fluidRow(
+    h1("Dois-je acheter une voiture electrique ?"),
+    p("Aujourd'hui on etend beaucoup parler des voitures électriques, notamment face
+      à l'urgence écologique. Est t'il le temps pour vous de passer au tout électrique
+      et laisser votre von vieux thermique de coté ?")
+  ),
+  
+  fluidRow(
+    img(src="audi_image_top.png",width = 439, height= 289),
+    ),
+  
+  
+  sidebarLayout(
+    sidebarPanel("test"),
+    mainPanel(
+      plotlyOutput(outputId = "evolution_plot")
+    )
+  ),
+  
   titlePanel("Borne de recharge selon le département"),
   
   sidebarLayout(
@@ -14,12 +33,6 @@ ui <- fluidPage(
     )
   ),
   
-  sidebarLayout(
-    sidebarPanel("test"),
-    mainPanel(
-        plotlyOutput(outputId = "evolution_plot")
-      )
-  )
   # titlePanel("test"),
   # mainPanel(
   #   plotlyOutput(outputId = "evolution_plot"))
