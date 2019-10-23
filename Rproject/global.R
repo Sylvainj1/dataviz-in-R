31993b7948923096dd1a93b0c3fc242f9f0eb21f
sylvainjiang <sylvainjiang1@gmail.com>
2019-10-23 14:01:06 +0200

    change file name and clean ui code

--- Rproject/global.R
+++ Rproject/global.R
@@ -6,7 +6,7 @@ library(ggplot2)
 library(dplyr)
 library(plotly)
 
-data = read.csv("data.csv",sep=';',header = TRUE,na.strings = ".")
+data = read.csv("bornedata.csv",sep=';',header = TRUE,na.strings = ".")
 data=data[complete.cases(data), ]
 data$Condition.d.accès=strtrim(data$Condition.d.accès,7)
 data=filter(data,Condition.d.accès=="gratuit" | Condition.d.accès =="payant")
