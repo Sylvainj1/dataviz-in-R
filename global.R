b88436a19a63a4d17e87480763d55219a3c71fcf
jeremys <jeremysurget77@gmail.com>
2019-10-23 14:37:21 +0200

    deleted unuse files

--- global.R
+++
@@ -1,7 +0,0 @@
-data = read.csv("data.csv",sep=';',header = TRUE,na.strings = ".")
-data=data[complete.cases(data), ]
-data$Condition.d.accès=strtrim(data$Condition.d.accès,7)
-data=filter(data,Condition.d.accès=="gratuit" | Condition.d.accès =="payant")
-
-data_evolution_station <- read.csv("nombre-total-de-points-de-charge.csv", sep=";",header = TRUE) %>% rename(Public = Accessible.au.public)
-data_evolution_station_long <- data_evolution_station %>% pivot_longer(c(Public,Particulier,Société),names_to = "Type", values_to = "Nombre")
